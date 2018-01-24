23326f76b8e836e9bd00782eb96ace780498cc9a
vroudge <v.roudge@gmail.com>
2018-01-24 15:20:57 +0100

    logs coldcuts

--- src/benders/coldcuts/index.js
+++ src/benders/coldcuts/index.js
@@ -57,6 +57,7 @@ export default class emile {
             await this.fillShippingInfos();
             logger.nfo(`filled shipping`);
             await this.page.click(`form > div.step__footer > button`);
+            logger.nfo(`in shipping page`);
             await this.page.waitFor(3000);
             await this.page.waitForSelector(`label > span.radio__label__accessory > span`);
 
