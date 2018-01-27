2d7ebf3b25e7acd05ffa56b114b9287b4dd2ecdc
vroudge <v.roudge@gmail.com>
2018-01-27 13:20:28 +0100

    FUCKING SIDEONE

--- src/benders/sideone/index.js
+++ src/benders/sideone/index.js
@@ -146,7 +146,7 @@ export default class sideone {
         await this.page.evaluate(() => {
             return ClNew.hideDialogMail();
         });
-
+        logger.nfo('done whole flow of filling')
         await this.page.waitFor(2000);
         await this.page.click(`#submit_noregister`);
     }
